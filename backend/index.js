1f4d830040f1d2ade213352c878996b0605807d0
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2025-01-03 18:29:14 +0530

    frontend - signup UI completed

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,16 @@ import dotenv from 'dotenv';
 import connectDB from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import messageRoute from './routes/message.router.js';
+import cors from 'cors';
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
 app.use('/api/auth',authRoute);
 app.use('/api/message',messageRoute);
 app.listen(PORT, () => {
